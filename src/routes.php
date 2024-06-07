f8eb2920796b2d8f07a1c43d1335cf966a9602a4
Matheus Brugge Milczwski <128723144+Brugge-Matheus@users.noreply.github.com>
2024-06-07 19:17:24 -0300

    changes

--- src/routes.php
+++ src/routes.php
@@ -13,8 +13,11 @@ $router->post('/login', 'LoginController@signinAction');
 $router->get('/register', 'LoginController@signup');
 $router->post('/register', 'LoginController@signupAction');
 
+$router->get('/profile/{id}', 'ProfileController@index');
+$router->get('/profile', 'ProfileController@index');
+
+
 $router->get('/search', '');
-$router->get('/profile', '');
 $router->get('/friends', '');
 $router->get('/photos', '');
 $router->get('/config', '');
