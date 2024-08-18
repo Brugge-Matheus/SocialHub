fe0751f7f19616680a8347aace8b505fb1f47b18
Brugge-Matheus <128723144+Brugge-Matheus@users.noreply.github.com>
2024-08-18 17:33:46 -0300

    new changes

--- public/views/login.php
+++ public/views/login.php
@@ -1,40 +1,33 @@
+<?php
+require '../../bootstrap.php';
+?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-        <meta
-            name="viewport"
-            content="width=device-width,minimum-scale=1,initial-scale=1"
-        />
-        <link rel="stylesheet" href="assets/css/login.css" />
-    </head>
-    <body>
-        <header>
-            <div class="container">
-                <a href=""><img src="assets/images/socialhub_logo.png" /></a>
-            </div>
-        </header>
-        <section class="container main">
-            <form method="POST">
-                <input
-                    placeholder="Digite seu e-mail"
-                    class="input"
-                    type="email"
-                    name="email"
-                />
 
-                <input
-                    placeholder="Digite sua senha"
-                    class="input"
-                    type="password"
-                    name="password"
-                />
+<head>
+    <meta charset="utf-8" />
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
+    <link rel="stylesheet" href="<?= BASE ?>assets/css/login.css" />
+</head>
 
-                <input class="button" type="submit" value="Acessar o sistema" />
+<body>
+    <header>
+        <div class="container">
+            <a href=""><img src="<?= BASE ?>assets/images/socialhub_logo.png" /></a>
+        </div>
+    </header>
+    <section class="container main">
+        <form method="POST" action="<?= BASE ?>login_action.php">
+            <input placeholder="Digite seu e-mail" class="input" type="email" name="email" />
 
-                <a href="">Ainda não tem conta? Cadastre-se</a>
-            </form>
-        </section>
-    </body>
+            <input placeholder="Digite sua senha" class="input" type="password" name="password" />
+
+            <input class="button" type="submit" value="Acessar o sistema" />
+
+            <a href="<?= BASE ?>signup.php">Ainda não tem conta? Cadastre-se</a>
+        </form>
+    </section>
+</body>
+
 </html>
